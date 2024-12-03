56a991fa7af7bbfb892124a55dadefe0fd41d9c6
Victor Schweitz <victorschweitz@hotmail.com>
2024-12-03 18:47:10 +0100

    HeroDescription set up

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,4 +1,5 @@
 import Section from './Section.jsx'
+import HeroDescription from './HeroDescription.jsx'
 import Image from 'next/image'
 
 export default function Hero() 
@@ -6,7 +7,11 @@ export default function Hero()
     return (
         <Section>
             <div className="h-[100%]">
+                <HeroDescription>
+                    <h2>This is the hero header</h2>
+                    <p>This is the hero paragraph</p>
+                </HeroDescription>
             </div>
         </Section>
-    ) 
+    )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 'use client'
-import Hero from '../app/components/Hero.jsx'
+
 
 export default function Home()
 {
   return (
-    <Hero>
-      
-    </Hero>
-    
+    <></>
+
   );
-  
+
 
 }
