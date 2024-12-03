16dfb77909b22a9dcb56584965f51fc7d06728ec
Victor Schweitz <victorschweitz@hotmail.com>
2024-12-03 17:33:32 +0100

    Logo set up

---
+++ src/app/components/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo({logoText}) 
+{
+    return (
+        <h3>
+            {logoText}
+        </h3>
+    ) 
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+import Logo from '../app/components/Logo.jsx';
+
+
 
 export default function Home() {
   return (
-<></>
+    <Logo logoText="FooFest" />
   );
 }
