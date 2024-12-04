51203c947ceb17080fdb461c7dd2a06c2f66316f
Victor Schweitz <victorschweitz@hotmail.com>
2024-12-04 19:43:14 +0100

    Formatted code

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -7,20 +7,20 @@ export default function Hero()
 {
     return (
         <Section>
-                <div className="h-[700px] full-width">
-                    <Image
-                        src={heroBg}
-                        alt="Hero background"
-                        layout="fill"
-                        className="object-cover h-full"
-                    />
-                    <div className="absolute bottom-12 right-12 text-right z-10 text-white">
-                        <HeroDescription>
-                            <h2>Band name</h2>
-                            <h4>Genre</h4>
-                        </HeroDescription>
-                    </div>
+            <div className="h-[700px] full-width">
+                <Image
+                    src={heroBg}
+                    alt="Hero background"
+                    layout="fill"
+                    className="object-cover h-full"
+                />
+                <div className="absolute bottom-12 right-12 text-right z-10 text-white">
+                    <HeroDescription>
+                        <h2>Band name</h2>
+                        <h4>Genre</h4>
+                    </HeroDescription>
                 </div>
+            </div>
         </Section>
     );
 }
