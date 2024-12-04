4310317d16814beac6fbdddc291715061c3492eb
Victor Schweitz <victorschweitz@hotmail.com>
2024-12-04 19:42:42 +0100

    Nothing new

---
+++ src/app/components/ImageSlider.jsx
@@ -0,0 +1,27 @@
+import Section from './Section.jsx'
+
+export default function ImageSlider() 
+{
+    const slides =
+        [
+            {
+                bandImage: '@/images/band_images/band_1.jpg',
+                bandName: 'Band 1'
+            },
+            {
+                bandImage: '@/images/band_images/band_2.jpg',
+                bandName: 'Band 2'
+            },
+            {
+                bandImage: '@/images/band_images/band_3.jpg',
+                bandName: 'Band 3'
+            },
+        ]
+
+        
+    return (
+        <Section>
+
+        </Section>
+    )
+}
