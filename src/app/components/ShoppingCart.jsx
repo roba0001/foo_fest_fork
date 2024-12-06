aaf9964e91e0c6edb5fd84b4da00e43ed3a17949
Ronja <roba0001@stud.kea.dk>
2024-12-06 13:10:06 +0100

    næsten done m shopping cart

--- src/app/components/ShoppingCart.jsx
+++ src/app/components/ShoppingCart.jsx
@@ -6,19 +6,31 @@ import { useState } from "react";
 
 export default function ShoppingCart() {
   const [title, setTitle] = useState("Shopping Cart");
-  const regularTotal = 24;
+  const [href, setHref] = useState("page.js");
+  const [buttonText, setButtonText] = useState("Go to payment");
+  const fixedBookingFee = 99;
+  // disse to skal kobles til antal biletter
+  const regularTotal = 40;
   const VIPTotal = 100;
   return (
     <Fieldset title={title}>
-      <div>
+      <div className="flex justify-between">
+        <p>Booking fee: </p>
+        <p>{fixedBookingFee}</p>
+      </div>
+      <div className="flex justify-between">
         <p>Regular pass: </p>
         <p>{regularTotal}</p>
       </div>
-      <div>
+      <div className="flex justify-between">
         <p>VIP pass: </p>
         <p>{VIPTotal}</p>
       </div>
-      <button>Go to payment</button>
+      <div className="flex justify-between">
+        <p>TOTAL: </p>
+        <p>{fixedBookingFee + regularTotal + VIPTotal}</p>
+      </div>
+      <Button href={href} buttonText={buttonText}></Button>
     </Fieldset>
   );
 }
