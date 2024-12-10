3f8e2355f3472ce17bc76a812219b9b05c245751
Victor Schweitz <victorschweitz@hotmail.com>
2024-12-10 12:56:17 +0100

    Imported GuestPassPriceCalculator

--- src/app/book/page.jsx
+++ src/app/book/page.jsx
@@ -4,10 +4,11 @@ import RootLayout from "../layout";
 import GuestInput from "../components/GuestInput";
 import Fieldset from "../components/Fieldset";
 import ShoppingCart from "../components/ShoppingCart";
-import ShoppingCart from "../components/ShoppingCart";
+import Navigation from "../components/Navigation";
 import GuestPassPriceCalculator from "../components/GuestPassPriceCalculator";
 
-export default function Book() {
+export default function Book()
+{
   const navItems = [
     {
       linkText: "Home",
@@ -24,21 +25,24 @@ export default function Book() {
   ];
 
   return (
-    <RootLayout>
-      <h1>BOOK YOUR STAY</h1>
-      <div className="grid grid-cols-2 grid-rows-2 mt-10">
-        <div className="col-start-2 sticky top-10">
-          <ShoppingCart />
+    <>
+      <Navigation navItems={navItems} />
+      <RootLayout>
+        <h1>BOOK YOUR STAY</h1>
+        <div className="grid grid-cols-2 grid-rows-2 mt-10">
+          <div className="col-start-2 sticky top-10">
+            <ShoppingCart />
+          </div>
+          <br />
+          <div className="col-start-1 row-start-1">
+            <Form>
+              <AreaInput />
+              <GuestPassPriceCalculator />
+              <GuestInput />
+            </Form>
+          </div>
         </div>
-        <br />
-        <div className="col-start-1 row-start-1">
-          <Form>
-            <AreaInput />
-            <GuestPassPriceCalculator />
-            <GuestInput />
-          </Form>
-        </div>
-      </div>
-    </RootLayout>
+      </RootLayout>
+    </>
   );
 }
