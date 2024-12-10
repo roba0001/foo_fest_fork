abeb736f00d0ee098ee7df39f9a8691ad7158429
Ronja <roba0001@stud.kea.dk>
2024-12-10 16:34:05 +0100

    styling af booksiden mostly done, mangler mobilesiden

--- src/app/book/page.jsx
+++ src/app/book/page.jsx
@@ -22,25 +22,33 @@ export default function Book() {
       href: "/book",
     },
   ];
+  let guests = [
+    { name: "Ronja", id: 1 },
+    { name: "Bonja", id: 2 },
+  ];
 
   return (
     <>
       <Navigation navItems={navItems} />
       <RootLayout>
         <h1>BOOK YOUR STAY</h1>
-        <div className="grid grid-cols-2 grid-rows-2 mt-10">
-          <div className="col-start-2 sticky top-10">
-            <ShoppingCart />
-          </div>
-          <br />
-          <div className="col-start-1 row-start-1">
-            <Form>
+        <Form>
+          <div className="grid grid-cols-3 grid-rows-2 mt-10 justify-center ">
+            <div className="flex flex-col col-span-2 row-start-1 col-start-1 self-center gap-10">
               <AreaInput />
-              <GuestPassPriceCalculator />
-              <GuestInput />
-            </Form>
+              {/* <GuestPassPriceCalculator /> */}
+
+              {guests.map((guest) => (
+                <GuestInput key={guest.id} guest={guest} />
+              ))}
+            </div>
+            <br />
+
+            <div className="col-start-3 row-start-1 sticky top-32">
+              <ShoppingCart />
+            </div>
           </div>
-        </div>
+        </Form>
       </RootLayout>
     </>
   );
--- src/app/components/Fieldset.jsx
+++ src/app/components/Fieldset.jsx
@@ -1,6 +1,6 @@
 export default function Fieldset({ children, title }) {
   return (
-    <fieldset className="flex flex-col self-center bg-white w-5/12 custom-border p-7">
+    <fieldset className="flex flex-col self-center bg-white w-3/5 custom-border p-7 ">
       <legend className="sr-only">{title}</legend>
       <h5 className="mb-3">{title}</h5>
 
