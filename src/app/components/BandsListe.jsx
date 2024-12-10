ab0352a6274aa91d7e3bb655b59dcd51308cc661
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-12-10 13:03:26 +0100

    b

--- src/app/components/BandsListe.jsx
+++ src/app/components/BandsListe.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 
+//
 export default function BandsList({ bands }) {
   const [schedule, setSchedule] = useState({});
   const [hoveredBand, setHoveredBand] = useState(null);
