32eb8b0519c349713afed98096cb3002e4777ca6
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-12-10 12:20:38 +0100

    b

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -1,49 +1,10 @@
-/*import { isRedirectError } from "next/dist/client/components/redirect";
-import RootLayout from "../layout";
-import BandCard from "@/app/components/BandCard";
-
-export default function Program() {
-  return (
-    <RootLayout>
-      <>
-        <BandCard />
-        <div>Program</div>
-      </>
-    </RootLayout>
-  );
-}
-  */
-/*
-import BandCard from "@/app/components/BandCard";
-import Skema from "@/app/components/Skema";
-
-export default function ProductsPage() {
-  return (
-    <div>
-      <BandCard />
-      <Skema />
-    </div>
-  );
-}
-  */
-/*
-import BandCard from "../components/BandCard";
-
-export default function Home() {
-  return (
-    <div>
-      <h1>Bandt eller scenerne </h1>
-      <BandCard />
-    </div>
-  );
-}
-*/
 import Category from "@/app/components/Category";
 import BandsListe from "@/app/components/BandsListe";
 
 export default function Home() {
   return (
     <div>
+      <h1>Bandt og scenerne på festival</h1>
       <Category />
       <BandsListe />
     </div>
