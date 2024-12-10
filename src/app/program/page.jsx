ac71fcae355d4ff00fa9499db575f8d5382edbb6
Astrid Marie Lindholm <asli0001@stud.kea.dk>
2024-12-10 12:37:05 +0100

    n

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -1,9 +1,26 @@
 import Category from "@/app/components/Category";
 import BandsListe from "@/app/components/BandsListe";
+import Navigation from "@/app/components/Navigation";
 
 export default function Home() {
+  const navItems = [
+    {
+      linkText: "Home",
+      href: "/",
+    },
+    {
+      linkText: "Program",
+      href: "/program",
+    },
+    {
+      linkText: "Book",
+      href: "/book",
+    },
+  ];
+
   return (
     <div>
+      <Navigation navItems={navItems} />
       <h1>Bandt og scenerne på festival</h1>
       <Category />
       <BandsListe />
