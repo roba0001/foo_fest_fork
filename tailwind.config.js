e922bbfec6adef52bd7d60bcee68d1cf7c075074
Ronja <roba0001@stud.kea.dk>
2024-12-10 13:27:20 +0100

    class bade for custom-border

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
         buttonBackgroundColor: "#fcac66",
         buttonHoverBackgroundColor: "#FF8922",
       },
+      // border: {
+      //   borderWidth: "border-2",
+      //   orangeBorderColor: "orange",
+      //   borderRadius3XL: "rounded-3xl",
+      //   padding: "p-7",
+      //   borderShadow: "shadow-xl",
+      // },
+
+      // border-2 border-orange-300 rounded-xl p-2
     },
   },
   plugins: [],
