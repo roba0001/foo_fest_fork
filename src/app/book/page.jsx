4dc2a1732a5ef9cb02fc6fcf3b3d9f10f0b57f4a
Ronja <roba0001@stud.kea.dk>
2024-12-11 13:47:47 +0100

    mangler kun radiobuttons farven

--- src/app/book/page.jsx
+++ src/app/book/page.jsx
@@ -7,6 +7,7 @@ import ShoppingCart from "../components/ShoppingCart";
 import Navigation from "../components/Navigation";
 import GuestPassPriceCalculator from "../components/GuestPassPriceCalculator";
 import { FiShoppingCart } from "react-icons/fi";
+import { IconContext } from "react-icons";
 
 export default function Book() {
   const navItems = [
@@ -28,14 +29,22 @@ export default function Book() {
     { name: "Bonja", id: 2 },
   ];
 
+  const style = { stroke: "orange", height: "2.5em", width: "2.5em" };
+
   return (
     <>
       <Navigation navItems={navItems} />
       <RootLayout>
         <h1>BOOK YOUR STAY</h1>
+
         <Form>
-          <div className="grid grid-cols-3 max-lg:grid-cols-1 grid-rows-2 mt-10 justify-start gap-10">
-            <div className="flex flex-col col-span-2 row-start-1 col-start-1  gap-10">
+          <div className="grid grid-cols-3 max-lg:grid-cols-1 grid-rows-2 mt-10 justify-start gap-10 mx-6">
+            <div className="col-start-2 row-start-1 sticky top-20  md:hide lg:hide">
+              <a href="#shoppingCart">
+                <FiShoppingCart style={style} />
+              </a>
+            </div>
+            <div className="flex flex-col md:col-span-2 lg:col-span-2 row-start-1 col-start-1  gap-10 justify-self-center">
               <AreaInput />
               {/* <GuestPassPriceCalculator /> */}
 
@@ -44,7 +53,10 @@ export default function Book() {
               ))}
             </div>
 
-            <div className="flex flex-col  col-start-3 md:row-start-1 lg:md:row-start-1 max-sm:col-start-1  md:sticky lg:sticky  ">
+            <div
+              id="shoppingCart"
+              className="flex sticky top-20 flex-col pt-5 justify-self-center  col-start-3 md:row-start-1 max-md:col-start-1 max-sm:col-start-1  md:sticky max-lg:sticky  "
+            >
               <ShoppingCart />
             </div>
           </div>
