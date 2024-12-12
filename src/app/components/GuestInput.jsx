43b39244006c26fb6edd2c3a181c4fe023d93b47
Ronja <roba0001@stud.kea.dk>
2024-12-12 11:11:07 +0100

    tilføj hover farve, og firstname lastname

--- src/app/components/GuestInput.jsx
+++ src/app/components/GuestInput.jsx
@@ -7,39 +7,54 @@ export default function GuestInput({ guest }) {
     <Fieldset title={`Guest #${guest.id} :`}>
       <div className="ml-3 flex flex-col gap-3">
         <div className="flex flex-col">
-          <label htmlFor="guestInput">
-            {" "}
-            <span className="text-red-500 font-bold">* </span>Name:{" "}
+          <label htmlFor="guestFirstName">
+            <span className="text-red-500 font-bold">* </span>First name:{" "}
           </label>
           <input
             className=" max-w-96 border-2 border-orange-300 rounded-xl p-2"
             type="text"
-            id="guestInput"
+            id="guestFirstName"
             name="guest"
-            autoComplete="name"
+            autoComplete="given-name"
             required
           />
         </div>
+
         <div className="flex flex-col">
-          <label htmlFor="guestInput">
+          <label htmlFor="guestLastName">
+            <span className="text-red-500 font-bold">* </span>Last name:{" "}
+          </label>
+          <input
+            className=" max-w-96 border-2 border-orange-300 rounded-xl p-2"
+            type="text"
+            id="guestLastName"
+            name="guest"
+            autoComplete="family-name"
+            required
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="guestEmail">
             {" "}
             <span className="text-red-500 font-bold">* </span>E-mail:{" "}
           </label>
           <input
             className=" max-w-72 border-2 border-orange-300 rounded-xl p-2"
             type="email"
-            id="guestInput"
+            id="guestEmail"
             name="guest"
             autoComplete="email"
             required
           />
         </div>
+
         <div className="flex flex-col">
-          <label htmlFor="guestInput">Phone: </label>
+          <label htmlFor="guestPhone">Phone: </label>
           <input
             className=" max-w-56 border-2 border-orange-300 rounded-xl p-2"
             type="tel"
-            id="guestInput"
+            id="guestPhone"
             name="guest"
             autoComplete="tel"
           />
