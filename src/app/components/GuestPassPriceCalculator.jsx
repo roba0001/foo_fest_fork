8760c677260c6039112aa2d8693e303ae85984b4
Victor Schweitz <victorschweitz@hotmail.com>
2024-12-15 10:02:15 +0100

    TentAmountChooser completed

--- src/app/components/GuestPassPriceCalculator.jsx
+++ src/app/components/GuestPassPriceCalculator.jsx
@@ -1,55 +1,266 @@
-'use client';
-import { useState, useMemo } from 'react';
-import Section from './Section';
-import TicketCounter from './TicketCounter';
-import TotalPriceDisplay from './TotalPriceDisplay';
-import TentAmountChooser from './TentAmountChooser';
+'use client'
+import { useState, useMemo, useEffect } from 'react'
+import Section from './Section'
+import TicketCounter from './TicketCounter'
+import TotalPriceDisplay from './TotalPriceDisplay'
+import TentAmountChooser from './TentAmountChooser'
+
+let twoPersonTentPrice = 299
+let threePersonTentPrice = 399
+
+/*
+  This function is being used as a helper function to calculate the price for each tent combination
+  It's being used for the "price" key in each object in the tentCombinations array
+*/
+function calculateTotalTentPrice(twoPersonTentAmount, threePersonTentAmount)
+{
+  return ((twoPersonTentAmount * twoPersonTentPrice) + (threePersonTentAmount * threePersonTentPrice))
+}
+
+/*
+    This array contains all essential information regarding the tent combinations:
+      1. selectedText: The text being displayed in the actual select dropdown for the given tent combination
+      2. optionText: The text being displayed in the select dropdown option for the given tent combination
+      3. twoPersonTentAmount: The amount of two-person tents needed
+      4. threePersonTentAmount: The amount of three-person tents needed
+      5. price: A function that calculates the total price for the given tent combination. It's using the twoPersonTentPrice and threePersonTentPrice variables. This function is used in the render method for the "TotalPriceDisplay" component.
+*/
+export const tentCombinations = [
+  {
+    selectText: '2 people',
+    optionText: '2 people (1 two-person tent)',
+    twoPersonTentAmount: 1,
+    threePersonTentAmount: 0,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '3 people',
+    optionText: '3 people (1 three-person tent)',
+    twoPersonTentAmount: 0,
+    threePersonTentAmount: 1,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '4 people',
+    optionText: '4 people (2 two-person tent)',
+    twoPersonTentAmount: 2,
+    threePersonTentAmount: 0,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '5 people',
+    optionText: '5 people (1 two-person tent + 1 three-person tent)',
+    twoPersonTentAmount: 1,
+    threePersonTentAmount: 1,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '6 people',
+    optionText: '6 people (2 three-person tents)',
+    twoPersonTentAmount: 3,
+    threePersonTentAmount: 0,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '6 people',
+    optionText: '6 people (3 two-person tents)',
+    twoPersonTentAmount: 0,
+    threePersonTentAmount: 2,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '7 people',
+    optionText: '7 people (2 two-person tents + 1 three-person tent)',
+    twoPersonTentAmount: 2,
+    threePersonTentAmount: 1,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '8 people',
+    optionText: '8 people (4 two-person tents)',
+    twoPersonTentAmount: 4,
+    threePersonTentAmount: 0,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '9 people',
+    optionText: '9 people (3 three-person tents)',
+    twoPersonTentAmount: 0,
+    threePersonTentAmount: 3,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '9 people',
+    optionText: '9 people (3 two-person tents + 1 three-person tent)',
+    twoPersonTentAmount: 3,
+    threePersonTentAmount: 1,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '10 people',
+    optionText: '10 people (5 two-person tents)',
+    twoPersonTentAmount: 5,
+    threePersonTentAmount: 0,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '11 people',
+    optionText: '11 people (1 two-person tent + 3 three-person tents)',
+    twoPersonTentAmount: 1,
+    threePersonTentAmount: 3,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '12 people',
+    optionText: '12 people (6 two-person tents)',
+    twoPersonTentAmount: 6,
+    threePersonTentAmount: 0,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  },
+  {
+    selectText: '12 people',
+    optionText: '12 people (3 two-person tents + 2 three-person tents)',
+    twoPersonTentAmount: 3,
+    threePersonTentAmount: 2,
+    price: function ()
+    {
+      return calculateTotalTentPrice(this.twoPersonTentAmount, this.threePersonTentAmount)
+    }
+  }
+]
 
 export default function GuestPassPriceCalculator()
 {
-  const regularTicketPrice = 799;
-  const vipTicketPrice = 1299;
+  // Setting up the price for each ticket type
+  const regularTicketPrice = 799
+  const vipTicketPrice = 1299
 
-  const [regularPriceCounter, setRegularPriceCounter] = useState(0);
-  const [vipPriceCounter, setVipPriceCounter] = useState(0);
-  const [optionalGreenCamping, setOptionalGreenCamping] = useState(false);
+  // Setting the price counters for each ticket type to 0 initially
+  const [regularPriceCounter, setRegularPriceCounter] = useState(0)
+  const [vipPriceCounter, setVipPriceCounter] = useState(0)
+
+  // Setting the option for optional green camping to false by default (meaning it has not been selected)
+  const [optionalGreenCamping, setOptionalGreenCamping] = useState(false)
+
+  // Setting the option for optional temt put up to false by default (meaning the option for choosing a tent combination is hidden by default)
   const [toggleOptionalTentPutUp, setToggleOptionalTentPutUp] = useState(false)
 
+  // Setting the selected tent combination to an empty string by default (since no tent combination option is selected initially)
+  const [selectedTent, setSelectedTent] = useState('')
+
+  // Setting the price for selected tent combination to 0 by default (since no tent combination option is selected initially)
+  const [selectedTentPrice, setSelectedTentPrice] = useState(0) // State for tent price
+
+  // Setting the total amount of tickets to be the result of the sum of the price of regular tickets and VIP tickets
+  const totalTickets = regularPriceCounter + vipPriceCounter
+
+  /*
+      Calculating the total price of tickets
+      If optional green camping is selected, then the price of that gets added to the total ticket price
+      The reason 99 is added to the return value of the basePrice is due to the single booking fee.
+  */
   const calculateTotalPrice = () =>
   {
     const basePrice =
       regularTicketPrice * regularPriceCounter +
       vipTicketPrice * vipPriceCounter +
-      99;
+      (optionalGreenCamping ? 249 : 0) +
+      selectedTentPrice
 
-    return optionalGreenCamping ? basePrice + 249 : basePrice; // Fix logic here
-  };
+    return basePrice + 99
 
-  // Dynamically calculate total price based on state
+  }
+
+  /*
+      Using the useMemo hook to only compute the value upon changing at least one of its dependencies
+  */
   const totalPrice = useMemo(calculateTotalPrice, [
     regularPriceCounter,
     vipPriceCounter,
     optionalGreenCamping,
-  ]);
+    selectedTentPrice, // Recalculate when tent price changes
+  ])
 
+  /*
+      Toggling the optional green camping to be the opposite of its previous state
+  */
   const handleOptionalGreenCamping = () =>
   {
-    setOptionalGreenCamping((prevState) => !prevState);
-  };
+    setOptionalGreenCamping((prevState) => !prevState)
+  }
 
+  /*
+      Toggling the optional tent put up to be the opposite of its previous state
+  */
   const handleOptionalTentPutUp = () =>
   {
     setToggleOptionalTentPutUp((prevState) => !prevState)
   }
 
+  /*
+      Using the useEffect hook to subtract the price of the currently selected tent combination option, if the user deselects the tent put up option entirely.
+  */
+  useEffect(() =>
+  {
+    if (!toggleOptionalTentPutUp)
+    {
+      setSelectedTentPrice(0) // Reset tent price
+      setSelectedTent("") // Clear selected tent
+    }
+  }, [toggleOptionalTentPutUp])
 
-
-
+  /*
+      Returning the entire GuestPassPriceCalculator component with all its child components
+  */
   return (
+    /*
+      Using the Section component as the main wrapper
+    */
     <Section>
       <div className="flex justify-center align-center items-center mx-auto w-fit px-12 py-4 h-fit gap-4 bg-white max-md:h-fit max-md:py-12">
         <div className="flex flex-col">
           <div className="flex gap-4 mb-8 max-md:flex-col">
+
+            {/* Creating an instance of the TicketCounter component. This one takes care of counting the regular tickets */}
             <TicketCounter
               ticketType="Regular"
               ticketPrice={regularTicketPrice}
@@ -64,6 +275,7 @@ export default function GuestPassPriceCalculator()
               }
               className="border-2 border-blue-500 rounded-md"
             />
+            {/* Creating an instance of the TicketCounter component. This one takes care of counting the VIP tickets */}
             <TicketCounter
               ticketType="VIP"
               ticketPrice={vipTicketPrice}
@@ -78,11 +290,9 @@ export default function GuestPassPriceCalculator()
               }
             />
           </div>
+          {/* The option to toggle optional green counting */}
           <div className="container optional-green-camping-container flex items-center gap-2 mb-2">
-            <label
-              htmlFor="optional_green_camping"
-              className="select-none cursor-pointer"
-            >
+            <label htmlFor="optional_green_camping" className="select-none cursor-pointer">
               Optional green camping (+ 249 DKK)
             </label>
             <input
@@ -91,19 +301,26 @@ export default function GuestPassPriceCalculator()
               className="accent-orange-300 cursor-pointer -order-1"
               id="optional_green_camping"
               onChange={handleOptionalGreenCamping}
-              checked={optionalGreenCamping} // Reflect current state
+              checked={optionalGreenCamping}
             />
           </div>
 
-          <div className={`container optional-tent-put-up-container select-none cursor-pointer ${regularPriceCounter + vipPriceCounter < 2
-            ? 'hidden'
-            : 'flex gap-2'
-            }`}>
+          {
+            /*
+              The optional tent put up container.
+              This container has been set up in a way, so that it only shows up, if the least amount of tickets selected is two, since the minimum amount of people for one tent must be two.
+            */
+          }
+          <div
+            className={`container optional-tent-put-up-container select-none cursor-pointer ${totalTickets < 2 ? "hidden" : "flex gap-2"
+              }`}
+          >
             <label
               htmlFor="optional_tent_put_up"
               className="select-none cursor-pointer"
             >
-              Optional tent put up (2P: DKK 299 - 3P: DKK 399 <small className="text-sm">(price is per tent)</small>)
+              Optional tent put up (2P: DKK 299 - 3P: DKK 399{" "}
+              <small className="text-sm">(price is per tent)</small>)
             </label>
             <input
               type="checkbox"
@@ -113,12 +330,29 @@ export default function GuestPassPriceCalculator()
               onChange={handleOptionalTentPutUp}
             />
           </div>
-          <div className={`${(regularPriceCounter + vipPriceCounter < 2 || !toggleOptionalTentPutUp) ? 'hidden' : 'flex gap-8 my-4'}`}>
-            <TentAmountChooser />
+
+          {
+            /*
+              The tent amount chooser.
+              This is set up in such a way that the it's being hidden, if the amount of total tickets is below 2, or the option to select put up has not been checked off.
+            */
+          }
+          <div
+            className={`${totalTickets < 2 || !toggleOptionalTentPutUp
+              ? "hidden"
+              : "flex gap-8 my-4"
+              }`}
+          >
+            <TentAmountChooser
+              numberOfTickets={totalTickets}
+              onTentPriceChange={setSelectedTentPrice} // Pass callback
+            />
           </div>
+
+          {/* Shows the total, computed price */}
           <TotalPriceDisplay totalPrice={totalPrice} />
         </div>
       </div>
     </Section>
-  );
+  )
 }
--- src/app/components/TentAmountChooser.jsx
+++ src/app/components/TentAmountChooser.jsx
@@ -1,18 +1,81 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { tentCombinations } from './GuestPassPriceCalculator'
+
+export default function ChooseTentAmount({ numberOfTickets, onTentPriceChange }) {
+    const [selectedTent, setSelectedTent] = useState('')
+    const [isDisabled, setIsDisabled] = useState(false)
+
+  
+    // Handle the tent selection
+    const handleTentCombinationSelect = (event) => {
+        const selectedOption = event.target.value
+        setSelectedTent(selectedOption)
+        setIsDisabled(true)
+
+        const selectedTentCombination = tentCombinations.find(
+            (tent) => tent.optionText === selectedOption
+        )
+
+        if (selectedTentCombination) {
+            const tentPrice = selectedTentCombination.price()
+            onTentPriceChange(tentPrice)
+        } else {
+            onTentPriceChange(0)
+        }
+    }
+
+    /*
+        Here, the useEffect hook is being used to let the tent combination select dropdown correspond to the change of ticket amount.
+
+    */
+    useEffect(() =>
+    {
+        // Reset dropdown when ticket count changes
+        setSelectedTent('')
+        setIsDisabled(false)
+
+        // Subtract price of current selection by letting the parent component know of the change
+        onTentPriceChange(0)
+    }, [numberOfTickets, onTentPriceChange])
 
 
-export default function ChooseTentAmount() 
-{
+    // Filter the available tent combinations
+    const availableTentCombinations = tentCombinations.filter((tentCombination) => {
+        const totalPeople =
+            tentCombination.twoPersonTentAmount * 2 +
+            tentCombination.threePersonTentAmount * 3
+        return totalPeople === numberOfTickets
+    })
+
+
     return (
-        <div className="container tent-amount-chooser-container flex gap-4 mb-4">
-           <div>
-               <label htmlFor="two_person_tents" className="block cursor-pointer">2P:</label>
-               <input type="number" className="tent-amount-input two-person-tents-amount-input rounded-md indent-[10px] ring-2 ring-orange-200 transition-ring duration-150 ease-in focus:outline-none focus:ring-2 focus:ring-orange-300" id="two_person_tents" placeholder="Enter tent amount"/>
-           </div>
-
-           <div>
-               <label htmlFor="three_person_tents" className="block cursor-pointer">3P:</label>
-               <input type="number" className="tent-amount-input two-person-tents-amount-input rounded-md indent-[10px] ring-2 ring-orange-200 transition-ring duration-150 ease-in focus:outline-none focus:ring-2 focus:ring-orange-300" id="three_person_tents" placeholder="Enter tent amount" />
-           </div>
+        <div>
+            <select
+                value={selectedTent}
+                onChange={handleTentCombinationSelect}
+                className="min-w-[300px] w-fit bg-transparent border-2 border-orange-300 p-2 rounded-md cursor-pointer"
+                disabled={isDisabled}
+            >
+                <option value=""  className="cursor-pointer" disabled>
+                    Select tent amount
+                </option>
+                {availableTentCombinations.length > 0 ? (
+                    availableTentCombinations.map((tentCombination, index) => (
+                        <option
+                            key={index}
+                            value={tentCombination.optionText}
+                            data-two_person_tent={tentCombination.twoPersonTentAmount}
+                            data-three_person_tent={tentCombination.threePersonTentAmount}
+                        >
+                            {tentCombination.optionText}
+                        </option>
+                    ))
+                ) : (
+                    <option disabled>No available tent combinations</option>
+                )}
+            </select>
         </div>
-    ) 
+    )
 }
