74d461511865577cc1ee78180da77a78cc450b86
Ronja <roba0001@stud.kea.dk>
2024-12-16 11:02:49 +0100

    put reserve-spot

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,30 +1,42 @@
+// supabase
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const headersList = {
+const supabaseHeadersList = {
   Accept: "application/json",
   "Content-Type": "application/json",
   apikey: key,
   Prefer: "return=representation",
 };
 
-export async function getInfo() {
-  const response = await fetch(url, {
-    method: "GET",
-    headers: headersList,
-  });
-
-  const data = await response.json();
-  return data;
-}
-
 export async function postInfo(formData) {
   const response = await fetch(url, {
     method: "POST",
-    headers: headersList,
+    headers: supabaseHeadersList,
     body: JSON.stringify(formData),
   });
 
   const data = await response.json();
   return data;
 }
+
+// FooFest API
+// put reserve spot
+const glitchHeadersList = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  Prefer: "return=representation",
+};
+
+export default async function putReservation(reservationData) {
+  const response = await fetch("http://localhost:8080/reserve-spot", {
+    method: "PUT",
+    headers: glitchHeadersList,
+    body: JSON.stringify(reservationData),
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+// post id
