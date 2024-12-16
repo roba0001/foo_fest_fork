c6def18b721bec59301013d699798a7a166d39dd
Ronja <roba0001@stud.kea.dk>
2024-12-16 12:13:26 +0100

    commit putrequest

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,3 +1,12 @@
+import putReservation from "@/lib/actions";
+
 export default function Form({ children }) {
-  return <form className="flex flex-col gap-5  justify-start">{children}</form>;
+  return (
+    <form action={putReservation} className="flex flex-col gap-5  justify-start">
+      {children}
+      <button className="bg-blue-200" type="submit">
+        Submit form
+      </button>
+    </form>
+  );
 }
